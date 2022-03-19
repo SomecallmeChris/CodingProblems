03131d0de1eaa58ee128e544e11ac6b1b3ab43f0
Christopher Reader <chrisjreader@Christophers-MacBook-Pro.local>
2022-03-19 12:08:46 +0000

    Add numeral validation and NumeralsToInteger methods

--- RomanNumerals/Program.cs
+++ RomanNumerals/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RomanNumerals
 {
     class Program
     {
-        static void Main(string[] args)
+        static Dictionary<char, int> numeralValues = new Dictionary<char, int>()
         {
-            Console.WriteLine("Hello World!");
+            {'M', 1000},
+            {'D', 500},
+            {'C', 100},
+            {'L', 50},
+            {'X', 10},
+            {'V', 5},
+            {'I', 1}
+        };
+
+        static int NumeralsToInteger(string numerals)
+        {
+            int result = 0;
+            int prev  = numeralValues.FirstOrDefault(x => x.Key == numerals[0]).Value;
+
+            foreach(char numeral in numerals)
+            {
+                var value  = numeralValues.FirstOrDefault(x => x.Key == numeral).Value;
+                if(value == prev)
+                    result += value;
+                Console.WriteLine($"Current: {value}, Previous : {prev}");
+                prev = value;
+
+            }
+
+            return result;
+        }
+
+        static bool NumeralValidation(string toValidate)
+        {
+            List<char> numerals = new List<char>()
+            {
+                'M', 'D', 'C', 'L', 'X', 'V', 'I'
+            };
+            foreach(char numeral in toValidate)
+            {
+                if(!numerals.Contains(numeral))
+                    return false;
+            }
+            return true;
+        }
+        public static void Main(string[] args)
+        {
+            string romanNumerals = "XXI";
+            //Console.WriteLine("Insert Roman Numerals: ");
+            //string romanNumerals = Console.ReadLine();
+            if(!NumeralValidation(romanNumerals))
+                Console.WriteLine("Input contains values not in the Roman Numeric System");
+            int output = NumeralsToInteger(romanNumerals);
+
+            //Console.WriteLine(output);
         }
     }
 }
